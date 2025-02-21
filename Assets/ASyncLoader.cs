7c9f41f3b393c7f84ed98377318bb00a26083ac2
gijm <chiemboesveld@gmail.com>
2025-02-21 11:31:06 +0100

    Je moeder dikke
    kanker neger

---
+++ Assets/ASyncLoader.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ASyncloader : MonoBehaviour
+{
+    [SerializeField] private GameObject
+    
+}
