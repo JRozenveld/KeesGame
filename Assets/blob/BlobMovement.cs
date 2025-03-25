6a155dd5dc27257157c913a5343066c871dc6f3d
Paskal1 <12217@staring.nl>
2025-03-25 17:17:24 +0100

    Revert "Merge branch 'Jens' of https://github.com/JRozenveld/KeesGame into Jens"
    This reverts commit 1a6f03bac80253735d55f6cfb1bdb0d841ded4cd, reversing
    changes made to 1320ea8f02b316dc30b2b0467f8c944c65452fbe.

--- Assets/blob/BlobMovement.cs
+++ Assets/blob/BlobMovement.cs
@@ -19,7 +19,7 @@ public class BlobMovement : MonoBehaviour
 
     void Update()
     {
-        // Converteer de muispositie van scherm- naar wereldcoÃ¶rdinaten
+        // Converteer de muispositie van scherm- naar wereldcoördinaten
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         targetPosition = new Vector2(mousePosition.x, mousePosition.y);
 
